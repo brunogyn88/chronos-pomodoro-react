b16e909743be8b35248ff4af5a7d7816591da363
Bruno Alves <brunogyn.eng@gmail.com>
2025-04-23 21:02:15 -0300

    Criacao do botao

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 function App() {
   return (
@@ -16,6 +20,27 @@ function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task:'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <Cycles />
+          </div>
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
       </Container>
     </>
   );
